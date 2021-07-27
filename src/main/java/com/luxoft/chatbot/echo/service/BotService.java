65261e136331dd9637b74b1560c7b7cb4b056af9
Maminspapin <y.lyulchenko@mail.ru>
2021-07-27 16:05:18 +0700

    Refactored service

--- src/main/java/com/luxoft/chatbot/echo/service/BotService.java
+++ src/main/java/com/luxoft/chatbot/echo/service/BotService.java
@@ -1,9 +1,29 @@
 package com.luxoft.chatbot.echo.service;
 
+import com.luxoft.chatbot.echo.dao.BotPropertyRepository;
 import com.luxoft.chatbot.echo.entity.BotProperties;
 import com.luxoft.chatbot.echo.exception.NoSuchBotPropertiesFound;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface BotService {
+import java.util.Optional;
 
-    BotProperties getBotProperties() throws NoSuchBotPropertiesFound;
+@Service
+public class BotService {
+
+    private final BotPropertyRepository botPropertyRepository;
+
+    @Autowired
+    private BotService(BotPropertyRepository botPropertyRepository) {
+        this.botPropertyRepository = botPropertyRepository;
+    }
+
+    public BotProperties getBotProperties() throws NoSuchBotPropertiesFound {
+        Optional<BotProperties> result = botPropertyRepository.findById(1);
+        if (result.isEmpty()) {
+            throw new NoSuchBotPropertiesFound("Properties for bot with id=1 is not found in DB. Check it!");
+        }
+
+        return result.get();
+    }
 }
