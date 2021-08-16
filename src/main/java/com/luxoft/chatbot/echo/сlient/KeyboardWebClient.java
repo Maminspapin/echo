f0c5073e51196965443fa17a5aa035a8b380f795
Maminspapin <y.lyulchenko@mail.ru>
2021-08-16 23:36:12 +0700

    Connect details to property

--- src/main/java/com/luxoft/chatbot/echo/сlient/KeyboardWebClient.java
+++ src/main/java/com/luxoft/chatbot/echo/сlient/KeyboardWebClient.java
@@ -2,6 +2,7 @@ package com.luxoft.chatbot.echo.сlient;
 
 import com.luxoft.chatbot.echo.dto.KeyboardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,18 +12,22 @@ import reactor.core.publisher.Mono;
 public class KeyboardWebClient {
 
     private final WebClient webClient;
+    private final String uri;
 
-    public KeyboardWebClient(@Autowired WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("http://localhost:8082").build();
+    public KeyboardWebClient(@Autowired WebClient.Builder builder,
+                             @Value("${service.keyboard.url}") String keyBoardServiceUrl,
+                             @Value("${service.keyboard.query.get.by-name}") String uri) {
+            this.webClient = builder.baseUrl(keyBoardServiceUrl).build();
+            this.uri = uri;
+        }
+
+        public Mono<KeyboardDTO> getKeyboard () {
+
+            return webClient
+                    .get()
+                    .uri(uri)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(KeyboardDTO.class);
+        }
     }
-
-    public Mono<KeyboardDTO> getKeyboard() {
-
-        return webClient
-                .get()
-                .uri("/api/keyboard/?name=test")
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(KeyboardDTO.class);
-    }
-}
